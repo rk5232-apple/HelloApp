18038b31ff31f8084ef8145d988a54aceb5c6b95
rk5232-apple <rk5232@srmist.edu.in>
2026-03-13 23:40:32 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }   
+        if (args.length > 0) {
+            String name = args[0]; // Getting the first argument
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no name is provided
+            System.out.println("Hello World!");
+        }
+    }
 }
