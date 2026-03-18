3b83e983eb2e8963b561ec9e1580a3de252723ba
rk5232-apple <rk5232@srmist.edu.in>
2026-03-18 13:15:55 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String name = args[0];
+        System.out.println("Hello ," + name +"!");
     }
 }
+
